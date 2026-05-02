afd695685322e2cee9a47b0c53b6c3b7c7689e30
just_got_zucced <just_got_zucced@noreply.codeberg.org>
2026-05-02 12:47:59 +0200

    Removed redundant comment
    Signed-off-by: just_got_zucced <just_got_zucced@noreply.codeberg.org>

--- tinyCompiler.cpp
+++ tinyCompiler.cpp
@@ -13,11 +13,6 @@ std::ostream& operator<<(std::ostream& os, TokenType type) {
 int main(int argc, char* argv[]) {
 	std::cout << "I'm just a compiler mate :p\n";
 
-	/*
-		refactor this into c++:
-		if len(sys.argv) != 2:
-		sys.exit("Error: Compiler needs source file as argument.")
-	*/
 	if (argc != 2) {
 		abort_("Error : Compiler needs source file as argument\n");
 	}
@@ -33,7 +28,6 @@ int main(int argc, char* argv[]) {
 	buffer << input.rdbuf();
 	std::string source{buffer.str()};
 	lex lexer(source);
-	//Emitter emitter("D:\\cs_prog\\tinyCompiler\\x64\\Debug\\out.c");
 	Emitter emitter("out.c");
 
 	Parser parser(lexer,emitter);
